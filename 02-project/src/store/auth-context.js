4a1ef155656b9896f045c9bd7bdaf1cb0bc39626
Arthh <arthur.ulhoa28@gmail.com>
2021-04-12 16:17:17 -0300

    feat: using AuthContext to login

---
+++ 02-project/src/store/auth-context.js
@@ -0,0 +1,36 @@
+import  {useEffect, useState, createContext } from 'react';
+
+const AuthContext = createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserStatus = localStorage.getItem('isLoggedIn');
+      if(storedUserStatus === '1'){
+        setIsLoggedIn(true);
+      }
+  },[]);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  }
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  }
+
+  return <AuthContext.Provider value={
+    {isLoggedIn: isLoggedIn,
+    onLogout: logoutHandler,
+    onLogin: loginHandler}
+    }>{props.children}</AuthContext.Provider>
+}
+
+export default AuthContext;
